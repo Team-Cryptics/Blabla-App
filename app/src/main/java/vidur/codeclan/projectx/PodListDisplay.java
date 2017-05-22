ea48dc5170d459eab1be5893b0584004276f5365
Sarthak <sarthakaggarwal97@gmail.com>
2017-05-22 23:11:58 +0530

    Attempt on second recycler

--- app/src/main/java/vidur/codeclan/projectx/PodListDisplay.java
+++ app/src/main/java/vidur/codeclan/projectx/PodListDisplay.java
@@ -7,34 +7,36 @@ import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class PodListDisplay extends AppCompatActivity {
+
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
-    ArrayList<InfoClass> list = new ArrayList<InfoClass>();
+    ArrayList<PodClass> list = new ArrayList<PodClass>();
     String[] image,heading,subheading,subdisp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.display_list_pod);
 
         image = getResources().getStringArray(R.array.image);
         heading = getResources().getStringArray(R.array.heading);
         subheading = getResources().getStringArray(R.array.subheading);
         subdisp = getResources().getStringArray(R.array.subdisp);
+
         int count = 0;
         for(String Heading : heading)
         {
-            InfoClass infoClass = new InfoClass(image[count],heading[count],subheading[count], subdisp[count]);
+            PodClass podClass = new PodClass(image[count],heading[count],subheading[count], subdisp[count]);
             count++;
-            list.add(infoClass);
+            list.add(podClass);
         }
 
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view1);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-        adapter = new InfoAdapter(list, this);
+        adapter = new PodAdapter(list, this);
         recyclerView.setAdapter(adapter);
 
     }
