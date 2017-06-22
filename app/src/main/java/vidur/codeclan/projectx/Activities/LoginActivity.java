976ec3c6951d548b9d903ced64bf6b77f864f12c
Samarth Gupta <samarthgupta1011@gmail.com>
2017-06-23 02:24:43 +0530

    Login UI

--- app/src/main/java/vidur/codeclan/projectx/Activities/LoginActivity.java
+++ app/src/main/java/vidur/codeclan/projectx/Activities/LoginActivity.java
@@ -2,13 +2,18 @@ package vidur.codeclan.projectx.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Response;
@@ -25,81 +30,66 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
-import vidur.codeclan.projectx.POJO.User;
 import vidur.codeclan.projectx.R;
 
 import static vidur.codeclan.projectx.POJO.GlobalAccess.URL_LOGIN;
-import static vidur.codeclan.projectx.POJO.GlobalAccess.currUser;
 
-public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "LoginActivity";
-    EditText etEmail, etPassword;
-    ImageButton btnLogin;
-    ProgressBar pbLoading;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvLogin,tvRegister;
+    View vvLogin,vvRegister;
+    LinearLayout llLogin, llRegister;
+    Button btLogin, btRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        etPassword = (EditText) findViewById(R.id.etPassword);
-        btnLogin = (ImageButton) findViewById(R.id.btnLogin);
-        pbLoading = (ProgressBar) findViewById(R.id.pbLoading);
+        tvLogin = (TextView) findViewById(R.id.tv_login);
+        tvRegister = (TextView) findViewById(R.id.tv_register);
+        vvLogin =  findViewById(R.id.vv_login);
+        vvRegister = findViewById(R.id.vv_register);
+        llLogin = (LinearLayout) findViewById(R.id.ll_login);
+        llRegister = (LinearLayout) findViewById(R.id.ll_register);
+        btLogin = (Button) findViewById(R.id.bt_login);
+        btRegister = (Button) findViewById(R.id.bt_register);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!etEmail.getText().toString().isEmpty() && ! etPassword.getText().toString().isEmpty()) {
-                    login();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        tvLogin.setOnClickListener(this);
+        tvRegister.setOnClickListener(this);
+        btLogin.setOnClickListener(this);
+        btRegister.setOnClickListener(this);
+
     }
 
-    public void login() {
-        btnLogin.setVisibility(View.GONE);
-        pbLoading.setVisibility(View.VISIBLE);
-        etEmail.setEnabled(false);
-        etPassword.setEnabled(false);
+    @Override
+    public void onClick(View view) {
 
-        Volley.newRequestQueue(this).add(new StringRequest(URL_LOGIN + "?q={%22filters%22:[{%22name%22:%22email%22,%22op%22:%22eq%22,%22val%22:%22" + etEmail.getText().toString() + "%22}]}",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        User localUser = new GsonBuilder().create().fromJson(response, User.class);
-                        if (localUser.getNumResults() == 0) {
-                            Toast.makeText(LoginActivity.this, "Email ID doesn't exist", Toast.LENGTH_SHORT).show();
+        switch (view.getId()) {
 
-                            btnLogin.setVisibility(View.VISIBLE);
-                            pbLoading.setVisibility(View.GONE);
-                            etEmail.setEnabled(true);
-                            etPassword.setEnabled(true);
-                        } else if (localUser.getObjects().get(0).getFollowers().get(0).getPwdhash().equals("true")) {
-                            Toast.makeText(LoginActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+            case R.id.tv_login:
+                llRegister.setVisibility(View.INVISIBLE);
+                llLogin.setVisibility(View.VISIBLE);
+                vvLogin.setVisibility(View.VISIBLE);
+                vvRegister.setVisibility(View.INVISIBLE);
+                break;
 
-                            btnLogin.setVisibility(View.VISIBLE);
-                            pbLoading.setVisibility(View.GONE);
-                            etEmail.setEnabled(true);
-                            etPassword.setEnabled(true);
-                        } else {
-                            currUser = localUser;
-                            startActivity(new Intent(LoginActivity.this, CategorySelectionActivity.class));
-                            finish();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(LoginActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            case R.id.tv_register:
+                llLogin.setVisibility(View.INVISIBLE);
+                llRegister.setVisibility(View.VISIBLE);
+                vvLogin.setVisibility(View.INVISIBLE);
+                vvRegister.setVisibility(View.VISIBLE);
+                break;
+            case R.id.bt_login :
 
-                btnLogin.setVisibility(View.VISIBLE);
-                pbLoading.setVisibility(View.GONE);
-                etEmail.setEnabled(true);
-                etPassword.setEnabled(true);
-            }
-        }));
+                break;
+
+            case R.id.bt_register :
+
+                break;
+
+        }
+
     }
 }
