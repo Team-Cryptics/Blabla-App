0d509401be94059be0d6839654a2d9ef17ac35bc
Sarthak <sarthakaggarwal97@gmail.com>
2017-06-28 00:17:27 +0530

    FB

--- app/src/main/java/vidur/codeclan/projectx/Activities/LoginActivity.java
+++ app/src/main/java/vidur/codeclan/projectx/Activities/LoginActivity.java
@@ -89,24 +89,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     }
                 });
 
-//        PackageInfo info;
-//        try {
-//            info = getPackageManager().getPackageInfo("vidur.codeclan.projectx", PackageManager.GET_SIGNATURES);
-//            for (Signature signature : info.signatures) {
-//                MessageDigest md;
-//                md = MessageDigest.getInstance("SHA");
-//                md.update(signature.toByteArray());
-//                String something = new String(Base64.encode(md.digest(), 0));
-//                //String something = new String(Base64.encodeBytes(md.digest()));
-//                Log.i("hash key", something);
-//            }
-//        } catch (PackageManager.NameNotFoundException e1) {
-//            Log.e("name not found", e1.toString());
-//        } catch (NoSuchAlgorithmException e) {
-//            Log.e("no such an algorithm", e.toString());
-//        } catch (Exception e) {
-//            Log.e("exception", e.toString());
-//        }
         sharedPreferences = getSharedPreferences("User", Context.MODE_PRIVATE);
         final String email = sharedPreferences.getString("email", null);
         final String passwd = sharedPreferences.getString("password", null);
